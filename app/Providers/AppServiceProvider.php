62e8ed68a7711601dfea32f28033ae5881731fea
Nazz05 <nguyenhoainam18105@gmail.com>
2026-04-06 15:40:42 +0700

    Fix Filament subpath Livewire URLs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $basePath = trim((string) parse_url((string) config('app.url'), PHP_URL_PATH), '/');
+        $prefix = $basePath === '' ? '' : '/' . $basePath;
+
+        $this->app->booted(function () use ($prefix): void {
+            Livewire::setScriptRoute(function ($handle) use ($prefix) {
+                return Route::get("{$prefix}/livewire/livewire.js", $handle);
+            });
+
+            Livewire::setUpdateRoute(function ($handle) use ($prefix) {
+                return Route::post("{$prefix}/livewire/update", $handle)->middleware('web');
+            });
+        });
     }
 }
